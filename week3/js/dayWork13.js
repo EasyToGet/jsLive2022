770f729e6f37878bdfcb5ad1d5dc9ef4efc5c5f4
Ed <yide1986@gmail.com>
2022-10-19 22:11:50 +0800

    feat: 新增 10/19 每日任務 Day13：物件操作

---
+++ week3/js/dayWork13.js
@@ -0,0 +1,23 @@
+/*
+以下是製作麵包的食譜，請新增一個名為 breadRecipe 的物件，並依照以下要求設定屬性：
+
+1. 新增一個名為 ingredients 的屬性，其值為一個陣列，包含 ＂高筋麵包粉＂、＂ 牛奶＂、＂酵母＂、＂糖＂、＂鹽＂、＂奶油＂。
+2. 新增一個名為 bakeTime 的屬性，其值為 30。
+3. 新增一個名為 difficulty 的屬性，其值為 ＂簡單＂。
+4. 新增一個名為 seasoning 的屬性，其值為一個物件，包含:
+    - 屬性名稱: sugar - 值:＂22g＂
+    - 屬性名稱: salt - 值:＂10g＂
+    - 屬性名稱: pepper - 值:＂10g＂
+*/
+
+let breadRecipe = {
+  /*  程式碼撰寫處  */
+  ingredients: ['高筋麵包粉', '牛奶', '酵母', '糖', '鹽', '奶油'],
+  bakeTime: 30,
+  difficulty: '簡單',
+  seasoning: {
+    sugar: '22g',
+    salt: '10g',
+    pepper: '10g'
+  }
+};
