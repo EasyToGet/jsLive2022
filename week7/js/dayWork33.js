e5a976fd1815da708f14dd69642999d88b4a5705
Ed <yide1986@gmail.com>
2022-12-03 22:48:34 +0800

    feat: 新增 11/16 每日任務 Day33：常見的 POST 請求

---
+++ week7/js/dayWork33.js
@@ -0,0 +1,13 @@
+/*
+  題目:
+  1. 請問瀏覽器預設的表單提交方式，其 Content-Type 類型是什麼?
+  2. 請問如果檔案為圖片、影片，則應該用哪一種 Content-Type 提交?
+  3. 請問如果資料為 JSON 格式，則應該用哪一種 Content-Type 提交?
+*/
+
+/*
+  答案:
+  1. application/x-www-form-urlencoded
+  2. multipart/form-data
+  3. application/json
+*/
