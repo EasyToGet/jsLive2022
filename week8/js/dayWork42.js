59f10f4fc8b13f24509d26084d1313a6572b520d
Ed <yide1986@gmail.com>
2022-12-20 01:31:38 +0800

    feat: 新增 11/29 每日任務 Day42：商品篩選功能 - 進階

---
+++ week8/js/dayWork42.js
@@ -0,0 +1,231 @@
+/*
+  題目: 
+  延續 Day 41 的每日任務，使用 這個 CodePen 練習。請嘗試不要將 select 欄位寫死，
+  而是使用 axios 取回的 products 資料，篩選出產品的 category 後再渲染至網頁上。
+*/
+
+const baseUrl = 'https://livejs-api.hexschool.io';
+const api_path = 'edjslive2022';
+
+const productList = document.querySelector('.productList');
+const cartList = document.querySelector('.cartList');
+
+let productData = [];
+let cartData;
+
+const getProducts = () => {
+  let url = `${baseUrl}/api/livejs/v1/customer/${api_path}/products`;
+  axios.get(url)
+    .then(response => {
+      productData = response.data.products;
+      renderProduct(productData);
+      getCategories();
+    }).catch(error => {
+      console.log(error);
+    })
+};
+
+const renderProduct = (productData) => {
+  let str = '';
+  productData.forEach(item => {
+    str += `<div class="col-6 mb-3">
+        <div class="card">
+          <img src=${item.images} class="card-img-top productImg" alt="產品圖片">
+          <div class="card-body">
+            <h5 class="card-title"><strong>標題:</strong> ${item.title}</h5>
+            <p class="card-text"><strong>種類:</strong> ${item.category}</p>
+            <p class="card-text"><strong>原始價格:</strong> ${item.origin_price}</p>
+            <p class="card-text"><strong>售價:</strong> ${item.price}</p>
+            <p class="card-text"><strong>描述:</strong> ${item.description}</p>
+            <button type="button" class="btn btn-primary addCart" data-id="${item.id}">加入購物車</button>
+          </div>
+        </div>
+      </div>`
+  });
+  productList.innerHTML = str;
+  let addCartBtn = document.querySelectorAll('.addCart');
+  addCartBtn.forEach(item => {
+    item.addEventListener('click', e => {
+      e.preventDefault();
+      addCart(e.target.dataset.id);
+    })
+  })
+};
+
+const getCart = () => {
+  let url = `${baseUrl}/api/livejs/v1/customer/${api_path}/carts`;
+  axios.get(url)
+    .then(response => {
+      cartData = response.data.carts;
+      renderCart(cartData);
+    }).catch(error => {
+      console.log(error);
+    })
+};
+
+const renderCart = (cartData) => {
+  let str = '';
+  cartData.forEach(item => {
+    str += `<li>
+        <p>名稱: <span>${item.product.title}</span></p>
+        <p>數量: <span>${item.quantity}</span></p>
+      </li>`
+  });
+  cartList.innerHTML = str;
+};
+
+const addCart = (id) => {
+  url = `${baseUrl}/api/livejs/v1/customer/${api_path}/carts`;
+  let data = {
+    "data": {
+      "productId": id,
+      "quantity": 1
+    }
+  };
+  axios.post(url, data)
+    .then(response => {
+      console.log(response);
+      getCart();
+    }).catch(error => {
+      console.log(error);
+    })
+};
+
+getProducts();
+getCart();
+
+// 商品篩選功能
+const productSelect = document.querySelector('.productSelect');
+productSelect.addEventListener('change', selectFilter);
+
+function selectFilter(e) {
+  e.preventDefault();
+  let category = e.target.value;
+  if (category === '全部') {
+    renderProduct(productData);
+    return;
+  }
+  let targetProducts = [];
+  productData.forEach(item => {
+    if (item.category === category) {
+      targetProducts.push(item);
+    };
+  });
+  renderProduct(targetProducts);
+}
+
+// 篩選 select 欄位教學
+const getCategories = () => {
+  // 步驟二: 分類前
+  let unSort = productData.map(item => {
+    return item.category
+  });
+  console.log(unSort);  // 結果為 ["床架","床架","窗簾","床架","收納","床架","收納","床架"]
+  //  步驟三: 分類後
+  let sorted = unSort.filter((item, i) => {
+    return unSort.indexOf(item) === i;
+  });
+  console.log(sorted);  // 結果為 ["床架","窗簾","收納"]
+  // 步驟四: 渲染至網頁上
+  renderCategories(sorted);
+};
+
+const renderCategories = (sorted) => {
+  let str = '<option value="全部" selected>全部</option>';
+  sorted.forEach(item => {
+    str += `<option value="${item}">${item}</option>`
+  });
+  productSelect.innerHTML = str;
+};
+
+//  送出訂單
+const addOrder = () => {
+  let url = `${baseUrl}/api/livejs/v1/customer/${api_path}/orders`;
+  let data = {
+    data: {
+      user: {
+        name: document.querySelector('#customerName').value.trim(),
+        tel: document.querySelector('#customerTel').value.trim(),
+        email: document.querySelector('#customerEmail').value.trim(),
+        address: document.querySelector('#customerAddress').value.trim(),
+        payment: document.querySelector('#payMethod').value.trim()
+      }
+    }
+  };
+  axios.post(url, data)
+    .then(response => {
+      console.log(response.data);
+      data = {};
+      getCart();
+    }).catch(error => {
+      console.log(error);
+    })
+};
+
+// 進階 : validate.js 驗證
+const constraints = {
+  "姓名": {
+    presence: {
+      message: "是必填欄位"
+    }
+  },
+  "電話": {
+    presence: {
+      message: "是必填欄位"
+    },
+    length: {
+      minimum: 8,
+      message: "號碼需超過 8 碼"
+    }
+  },
+  "信箱": {
+    presence: {
+      message: "是必填欄位"
+    },
+    email: {
+      message: "格式有誤"
+    }
+  },
+  "地址": {
+    presence: {
+      message: "是必填欄位"
+    }
+  }
+};
+const form = document.querySelector('.submitForm');
+const inputs = document.querySelectorAll('input[type=text], input[type=tel]');
+// 取得所有帶有 data-msg 的 <p> 標籤
+const messages = document.querySelectorAll('[data-msg]');
+// 綁定整個 form 表單，驗證成功才准許送出表單
+form.addEventListener('submit', verification, false);
+function verification(e) {
+  e.preventDefault();
+  let errors = validate(form, constraints);
+  // 如果有誤，呈現錯誤訊息 
+  if (errors) {
+    showErrors(errors);
+  } else {
+    // 如果沒有錯誤，送出表單
+    addOrder();
+  }
+};
+
+const showErrors = (errors) => {
+  messages.forEach(item => {
+    item.textContent = '';
+    item.textContent = errors[item.dataset.msg];
+  })
+};
+// 監控所有 input 的操作
+inputs.forEach(item => {
+  item.addEventListener('change', (e) => {
+    e.preventDefault();
+    let targetName = item.name;
+    let errors = validate(form, constraints);
+    item.nextElementSibling.textContent = '';
+    // 針對正在操作的欄位呈現警告訊息
+    if (errors) {
+      document.querySelector(`[data-msg='${targetName}']`).textContent = errors[targetName];
+    }
+  });
+});
